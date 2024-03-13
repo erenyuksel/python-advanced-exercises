05bc6beffab6c33fbd8eb5a75727feeb6072ffec
Your Name <you@example.com>
2024-03-13 10:23:45 +0100

    functions

---
+++ online_shop.py
@@ -0,0 +1,31 @@
+def compute_totals(orders):
+    result = []
+    for order in orders:
+        total_price = order['quantity'] * order['price_per_item']
+        if total_price < 100:
+            total_price += 10
+        result.append((order['id'], total_price))
+    return result
+orders = [
+    {
+        'id': 'order_001',
+        'item': 'Introduction to Python',
+        'quantity': 1,
+        'price_per_item': 32,
+    },
+    {
+        'id': 'order_002',
+        'item': 'Advanced Python',
+        'quantity': 3,
+        'price_per_item': 40,
+    },
+    {
+        'id': 'order_003',
+        'item': 'Python web frameworks',
+        'quantity': 2,
+        'price_per_item': 51,
+    },
+]
+
+totals = compute_totals(orders)
+print(totals)
---
+++ squared_even_numbers.py
@@ -0,0 +1,6 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+even_numbers = list((map(lambda x: x**2,filter(lambda x: x % 2 == 0, numbers))))
+print(even_numbers)
+
+
